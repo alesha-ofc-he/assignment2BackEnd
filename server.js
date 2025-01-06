719e89acbb17f721fc1580d75a79c516234ff6b3
Arman Issayev <issayevarman@mail.ru>
2025-01-06 08:00:54 +0500

    =govno

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const weather = require('./public/weather');
+const weather = require('./public/weather')
 
 app.use(express.json())
 
